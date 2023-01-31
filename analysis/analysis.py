1fb304beba37e15cf0fc681c5dc3f40b50a1b8ac
Alice Girard Guittard <hi@alicegg.tech>
2023-01-31 16:58:22 +0100

    Better graph

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -2,10 +2,10 @@ import pandas as pd
 import plotext as plt
 
 df = pd.read_csv('out.csv')
-durations = df[df.metric_name == 'http_req_duration'].set_index('timestamp').groupby('timestamp')
-p90 = durations['metric_value'].transform(lambda x: x.quantile(0.90))
-print(p90)
+p90 = df[df.metric_name == 'http_req_duration'].groupby('timestamp').metric_value.agg(lambda x: x.quantile(0.90))
+rps = df[df.metric_name == 'http_reqs'].groupby('timestamp').size()
 
-plt.plot(p90)
+plt.plot(p90.index, p90, label="p90 duration", yside="left")
+plt.plot(rps.index, rps, label="Request per second", yside="right")
 plt.title("p90 durations")
 plt.show()
